a53739ff778dfed1786b42b33e6d9f68acb64fd9
MaharaniNab <maharani91104@gmail.com>
2025-09-12 18:02:43 +0700

    fix vite base path and vercel routes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -9,9 +10,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: true, // biar bisa diakses via IP LAN
+    port: 5173, // default Vite
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-  base: './', // ✅ penting untuk Vercel supaya asset path benar
-})
+  base: './', 
